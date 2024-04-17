72430cfdd1e44185053f6a324d008a2cd05954fe
Pavannkurapati03 <pavankurapati0105@gmail.com>
2024-04-17 11:46:49 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,13 @@
+import os
 import sys
-import logging
+
+# Use a raw string for the file path
+sys.path.append(os.path.abspath(r'C:\Users\Pavan\Documents\mlproject\src'))
+
+from logger import logging
+
+
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,6 +26,18 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+
+    try :
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
+
+        
+
+
     
 
 
